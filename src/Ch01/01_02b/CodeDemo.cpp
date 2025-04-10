88da96c32289bda18afe2dc3eeabc560260100f9
Spencer Easton <115196591+sreaston@users.noreply.github.com>
2025-04-10 20:50:22 +0000

    hello

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    
+    std::cout << "Hi there!" << std::endl;
+    std::cout << std::endl << std::endl;
+
+    return(0);
+}
