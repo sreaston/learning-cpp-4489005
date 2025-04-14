ba89b70e0f4e9664d84407e975fb471a83b54b09
Spencer Easton <115196591+sreaston@users.noreply.github.com>
2025-04-14 22:09:58 +0000

    commit

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,22 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+#define CAPACITY 5000
+//#define DEBUG
 
 int main(){
     
+    int32_t large = CAPACITY;
+#ifdef DEBUG
+    std::cout << "!!Begin addition!!" << std::endl;
+#endif
+    uint8_t small = 37;
+    large += small;
+    std::cout << "Large = " << large << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
