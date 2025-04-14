b920fc78a68b623127c90c52e2349ebed7a43240
Spencer Easton <115196591+sreaston@users.noreply.github.com>
2025-04-14 21:26:06 +0000

    commit

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,28 @@
 
 #include <iostream>
 
+int a, b = 5; // single line comment
+
+/*Multi
+Line
+Comment
+*/
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    bool myFlag;
+    a = 7;
+    myFlag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << myFlag << std::endl;
+    myFlag = true;
+    std::cout << "flag = " << myFlag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
